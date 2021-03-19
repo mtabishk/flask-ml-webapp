118fafd64c12c8e2b5352c7634f27da21a1e38d6
root <root@ip-172-31-5-222.ap-south-1.compute.internal>
2021-03-19 18:39:36 +0000

    updated

--- flask-app_dl/app.py
+++ flask-app_dl/app.py
@@ -1,5 +1,6 @@
 from keras.models import load_model
 from flask import Flask, render_template, request
+import json
 
 model = load_model("diabetes_model.h5")
 app = Flask("diabetes_model_app")
@@ -11,7 +12,7 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-	no_pregnent = float(request.form['no_pregnent'])	
+	no_pregnant = float(request.form['no_pregnant'])	
 	plasma_glucose_conc = float(request.form['plasma_glucose_conc'])	
 	diastolic_bp = float(request.form['diastolic_bp'])	
 	triceps_skinfold_thickness = float(request.form['triceps_skinfold_thickness'])	
@@ -20,7 +21,7 @@ def predict():
 	diabetes_pedigree_fn = float(request.form['diabetes_pedigree_fn'])	
 	age = float(request.form['age'])
 	
-	output = model.predict( [[no_pregnent, plasma_glucose_conc, diastolic_bp, triceps_skinfold_thickness, serum_insulin, bmi, diabetes_pedigree_fn, age
+	output = model.predict( [[no_pregnant, plasma_glucose_conc, diastolic_bp, triceps_skinfold_thickness, serum_insulin, bmi, diabetes_pedigree_fn, age
 ] ])
 	if str(round(output[0][0])) == '0':
 		prediction_text = 'Dead'
@@ -30,6 +31,27 @@ def predict():
 	return  render_template("result.html", prediction_text= prediction_text )
  
 
+@app.route("/api/predict", methods=["GET"])
+def predict_api():
+        no_pregnant = float(request.args['no_pregnant'])
+        plasma_glucose_conc = float(request.args['plasma_glucose_conc'])
+        diastolic_bp = float(request.args['diastolic_bp'])
+        triceps_skinfold_thickness = float(request.args['triceps_skinfold_thickness'])
+        serum_insulin = float(request.args['serum_insulin'])
+        bmi = float(request.args['bmi'])
+        diabetes_pedigree_fn = float(request.args['diabetes_pedigree_fn'])
+        age = float(request.args['age'])
+
+        output = model.predict( [[no_pregnant, plasma_glucose_conc, diastolic_bp, triceps_skinfold_thickness, serum_insulin, bmi, diabetes_pedigree_fn, age
+] ])
+        if str(round(output[0][0])) == '0':
+                prediction_text = 'Dead'
+        else:
+                prediction_text = 'Alive'
+    
+        output = {"status": f"{prediction_text}"}
+        return  json.dumps(output)
+
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000, debug=True)
+	app.run(host="0.0.0.0", port=8080)
